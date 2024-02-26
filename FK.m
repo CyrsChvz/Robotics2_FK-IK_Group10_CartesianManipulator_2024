4bc7d01b4a1d98b2fb2c3988e6fcb1f379352058
Arago, Allan Jhasper P <157667865+JustJhz@users.noreply.github.com>
2024-02-26 22:11:26 +0800

    Update FK.m

--- FK.m
+++ FK.m
@@ -2,4 +2,11 @@
 
 ## Link Lenghts in mm
 
-a1 = float(input (
+a1 = float(input("a1 = "))
+a2 = float(input("a2 = "))
+a3 = float(input("a3 = "))
+a4 = float(input("a4 = "))
+
+## Joint Variables: Is mm if f
+
+d1 = float
