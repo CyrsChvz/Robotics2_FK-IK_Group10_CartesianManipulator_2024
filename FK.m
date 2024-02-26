447f51a33a42fda3c59925c470c42d570ee262e5
Arago, Allan Jhasper P <157667865+JustJhz@users.noreply.github.com>
2024-02-26 22:14:25 +0800

    Update FK.m

--- FK.m
+++ FK.m
@@ -9,4 +9,8 @@ a4 = float(input("a4 = "))
 
 ## Joint Variables: Is mm if f
 
-d1 = float
+d1 = float(input("d1 = ")) #20 mm
+d2 = float(input("d2 = ")) #10 mm
+d3 = float(input("d3 = ")) # 15 mm
+
+## 
