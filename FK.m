90664271aa49454b1986e41c50527deb2fa265d8
Cyrus Chavez <157597327+CyrsChvz@users.noreply.github.com>
2024-02-27 10:17:21 +0800

    Update FK.m

--- FK.m
+++ FK.m
@@ -5,3 +5,6 @@ a1 = float(input("a1 = "))
 a2 = float(input("a2 = "))
 a3 = float(input("a3 = "))
 a4 = float(input("a4 = "))
+
+# joint variables: is mm if f, is degrees if theta
+d1 = float(input("d1 = ")) #20 mm
