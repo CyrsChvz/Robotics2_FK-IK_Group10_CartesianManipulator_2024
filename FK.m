be5a862c45fe41fe26c91a7b0feae27971bacab3
Cyrus Chavez <157597327+CyrsChvz@users.noreply.github.com>
2024-02-27 10:24:48 +0800

    Update FK.m

--- FK.m
+++ FK.m
@@ -27,3 +27,10 @@ H0_1 = [[np.cos(PT[i][0]),-np.sin(PT[i][0])*np.cos(PT[i][1]),np.sin(PT[i][0])*np
 i = 1
 H1_2 = [[np.cos(PT[i][0]),-np.sin(PT[i][0])*np.cos(PT[i][1]),np.sin(PT[i][0])*np.sin(PT[i][1]),PT[i][2]*np.cos(PT[i][0])],
         [np.sin(PT[i][0]),np.cos(PT[i][0])*np.cos(PT[i][1]),-np.cos(PT[i][0])*np.sin(PT[i][1]),PT[i][2]*np.sin(PT[i][0])],
+        [0,np.sin(PT[i][1]),np.cos(PT[i][1]),PT[i][3]],
+        [0,0,0,1]]
+
+i = 2
+H2_3 = [[np.cos(PT[i][0]),-np.sin(PT[i][0])*np.cos(PT[i][1]),np.sin(PT[i][0])*np.sin(PT[i][1]),PT[i][2]*np.cos(PT[i][0])],
+        [np.sin(PT[i][0]),np.cos(PT[i][0])*np.cos(PT[i][1]),-np.cos(PT[i][0])*np.sin(PT[i][1]),PT[i][2]*np.sin(PT[i][0])],
+        
