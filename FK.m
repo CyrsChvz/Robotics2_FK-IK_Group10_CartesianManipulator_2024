dd517ca12708a66ff99453d4b209d703b9ec175a
Cyrus Chavez <157597327+CyrsChvz@users.noreply.github.com>
2024-02-27 10:22:28 +0800

    Update FK.m

--- FK.m
+++ FK.m
@@ -10,3 +10,10 @@ a4 = float(input("a4 = "))
 d1 = float(input("d1 = ")) #20 mm
 d2 = float(input("d2 = "))
 d3 = float(input("d3 = "))
+
+# Parametric Table (theta, alpha, r, d)
+PT = [[(0.0/180.0)*np.pi,(90.0/180.0)*np.pi,0,a1],
+      [(90.0/180.0)*np.pi,(90.0/180.0)*np.pi,0,a2+d1],
+      [(270.0/180.0)*np.pi,(90.0/180.0)*np.pi,0,a3+d2],
+      [(0.0/180.0)*np.pi,(0.0/180.0)*np.pi,0,a4+d3]]
+      
